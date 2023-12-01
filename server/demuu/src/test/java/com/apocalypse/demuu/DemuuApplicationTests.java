739b395bbe6affb61b5608c7ac79270a7a95b7bc
demuu <preasim.kr@gmail.com>
2023-12-01 22:04:42 +0900

    first commit

---
+++ server/demuu/src/test/java/com/apocalypse/demuu/DemuuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apocalypse.demuu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemuuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
