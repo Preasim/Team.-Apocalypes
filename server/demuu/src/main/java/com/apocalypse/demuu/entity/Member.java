19020e7cd72699e5ce68a9054b48122e6da67049
demuu <preasim.kr@gmail.com>
2023-12-03 23:24:05 +0900

    Update Member.java

--- server/demuu/src/main/java/com/apocalypse/demuu/entity/Member.java
+++ server/demuu/src/main/java/com/apocalypse/demuu/entity/Member.java
@@ -1,5 +1,45 @@
 package com.apocalypse.demuu.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@Entity
 public class Member {
-    int hello;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private long memberCode;
+
+    private String image;
+
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.MEMBER_ACTIVE;
+
+    private Timestamp createdAt;
+
+    private Timestamp modifiedAt;
+
+
+    public enum Status {
+        MEMBER_ACTIVE("활동"),
+        MEMBER_DELETE("탈퇴");
+
+        private String status;
+
+        Status(String status) {
+            this.status = status;
+        }
+    }
 }
