92419112236152103f653d2650012dd16831be16
demuu <preasim.kr@gmail.com>
2023-12-03 23:23:56 +0900

    Create ExceptionCode.java

---
+++ server/demuu/src/main/java/com/apocalypse/demuu/exception/ExceptionCode.java
@@ -0,0 +1,19 @@
+package com.apocalypse.demuu.exception;
+
+import lombok.Getter;
+
+public enum ExceptionCode {
+    MEMBER_NOT_FOUND(404, "회원을 찾을 수 없습니다"),
+    MEMBER_EXISTS(409, "회원이 존재합니다"),
+    MEMBER_IS_DELETED(404, "탈퇴한 회원입니다");
+
+    @Getter
+    private int status;
+
+    @Getter
+    private String message;
+    ExceptionCode(int code, String message) {
+        this.status = code;
+        this.message = message;
+    }
+}
