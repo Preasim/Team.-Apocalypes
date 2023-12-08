f835ba62ea635431fde66818500187419b5423b1
demuu <preasim.kr@gmail.com>
2023-12-09 04:23:53 +0900

    Create SingleResponseDto.java

---
+++ server/demuu/src/main/java/com/apocalypse/demuu/response/SingleResponseDto.java
@@ -0,0 +1,10 @@
+package com.apocalypse.demuu.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SingleResponseDto<T> {
+    private T data;
+}
