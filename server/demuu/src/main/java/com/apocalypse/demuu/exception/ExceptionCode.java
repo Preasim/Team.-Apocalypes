763593c6e5b3374d38740f5aab6748d55a3b04e5
demuu <preasim.kr@gmail.com>
2023-12-09 04:24:01 +0900

    Update ExceptionCode.java

--- server/demuu/src/main/java/com/apocalypse/demuu/exception/ExceptionCode.java
+++ server/demuu/src/main/java/com/apocalypse/demuu/exception/ExceptionCode.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 public enum ExceptionCode {
     MEMBER_NOT_FOUND(404, "회원을 찾을 수 없습니다"),
     MEMBER_EXISTS(409, "회원이 존재합니다"),
-    MEMBER_IS_DELETED(404, "탈퇴한 회원입니다");
+    MEMBER_IS_DELETED(404, "탈퇴한 회원입니다"),
+    NAME_EXISTS(409, "이미 존재하는 이름입니다.");
 
     @Getter
     private int status;
