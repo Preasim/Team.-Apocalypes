085b9f2be2e42e9b6312ef4229e68f911c117a6c
demuu <preasim.kr@gmail.com>
2023-12-09 04:23:56 +0900

    Create PageInfo.java

---
+++ server/demuu/src/main/java/com/apocalypse/demuu/response/PageInfo.java
@@ -0,0 +1,15 @@
+package com.apocalypse.demuu.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class PageInfo {
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+}
