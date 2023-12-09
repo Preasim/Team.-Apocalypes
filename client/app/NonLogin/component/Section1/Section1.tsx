b4089c9b6d384129da9fc267c1eed031f7439e04
fefdfea <cankyu6@gmail.com>
2023-12-09 17:46:28 +0900

    경로오류 수정

--- client/app/NonLogin/component/Section1/Section1.tsx
+++ client/app/NonLogin/component/Section1/Section1.tsx
@@ -1,4 +1,4 @@
-import Section1FirstScene from "./Section1FirstScene/section1FirstScene";
+import Section1FirstScene from "./Section1FirstScene/Section1FirstScene";
 import Section1SecondScene from "./Section1SecondScene/Section1SecondScene";
 
 export default function Section1() {
