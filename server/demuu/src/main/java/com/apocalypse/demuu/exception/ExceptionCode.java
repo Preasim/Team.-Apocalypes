5582a899e7802961e65c4ec0eaf75c370ef3a232
demuu <preasim.kr@gmail.com>
2023-12-10 23:42:05 +0900

    Update ExceptionCode.java

--- server/demuu/src/main/java/com/apocalypse/demuu/exception/ExceptionCode.java
+++ server/demuu/src/main/java/com/apocalypse/demuu/exception/ExceptionCode.java
@@ -6,6 +6,7 @@ public enum ExceptionCode {
     MEMBER_NOT_FOUND(404, "회원을 찾을 수 없습니다"),
     MEMBER_EXISTS(409, "회원이 존재합니다"),
     MEMBER_IS_DELETED(404, "탈퇴한 회원입니다"),
+    NO_PERMISSION_EDITING_POST(403,"작성자만 수정할 수 있습니다"),
     NAME_EXISTS(409, "이미 존재하는 이름입니다.");
 
     @Getter
