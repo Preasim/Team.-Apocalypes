40c75549b504838738a3acefdfebc7e1b972deaa
demuu <preasim.kr@gmail.com>
2023-12-10 23:42:09 +0900

    Update MailController.java

--- server/demuu/src/main/java/com/apocalypse/demuu/controller/MailController.java
+++ server/demuu/src/main/java/com/apocalypse/demuu/controller/MailController.java
@@ -1,21 +1,21 @@
-package com.apocalypse.demuu.controller;
-
-import com.apocalypse.demuu.service.MailService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-@Controller
-@RequiredArgsConstructor
-public class MailController {
-    private final MailService mailService;
-
-    @ResponseBody
-    @PostMapping("/mail")
-    public String MailSend(String mail) {
-        int number = mailService.sendMail(mail);
-        String num = "" + number;
-        return num;
-    }
-}
+//package com.apocalypse.demuu.controller;
+//
+//import com.apocalypse.demuu.service.MailService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//@Controller
+//@RequiredArgsConstructor
+//public class MailController {
+//    private final MailService mailService;
+//
+//    @ResponseBody
+//    @PostMapping("/signup/mail")
+//    public String MailSend(String mail) {
+//        int number = mailService.sendMail(mail);
+//        String num = "" + number;
+//        return num;
+//    }
+//}
