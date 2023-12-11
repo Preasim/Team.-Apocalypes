ef450ae9018bf9d9d35012e812fa31be6cf754f8
demuu <preasim.kr@gmail.com>
2023-12-12 04:05:51 +0900

    Update DemuuApplication.java

--- server/demuu/src/main/java/com/apocalypse/demuu/DemuuApplication.java
+++ server/demuu/src/main/java/com/apocalypse/demuu/DemuuApplication.java
@@ -2,7 +2,9 @@ package com.apocalypse.demuu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DemuuApplication {
 
