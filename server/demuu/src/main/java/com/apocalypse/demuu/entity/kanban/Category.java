3d102b6ce11b5cb7761d72fb0e250b37759a880f
demuu <preasim.kr@gmail.com>
2023-12-16 22:49:59 +0900

    Create Category.java

---
+++ server/demuu/src/main/java/com/apocalypse/demuu/entity/kanban/Category.java
@@ -0,0 +1,41 @@
+package com.apocalypse.demuu.entity.kanban;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+
+    @ManyToOne
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    private String categoryName;
+
+    @Column(updatable = false)
+    private Timestamp createdAt;
+
+    private Timestamp modifiedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = new Timestamp(System.currentTimeMillis());
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.modifiedAt = new Timestamp(System.currentTimeMillis());
+    }
+}
