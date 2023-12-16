54cd139d49a0f83392a3dc5685531fa8cfeec7ab
demuu <preasim.kr@gmail.com>
2023-12-16 22:50:01 +0900

    Create Board.java

---
+++ server/demuu/src/main/java/com/apocalypse/demuu/entity/kanban/Board.java
@@ -0,0 +1,43 @@
+package com.apocalypse.demuu.entity.kanban;
+
+import com.apocalypse.demuu.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long boardId;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private String boardName;
+
+    @Column(updatable = false)
+    private Timestamp createdAt;
+
+    private Timestamp modifiedAt;
+
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = new Timestamp(System.currentTimeMillis());
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.modifiedAt = new Timestamp(System.currentTimeMillis());
+    }
+}
