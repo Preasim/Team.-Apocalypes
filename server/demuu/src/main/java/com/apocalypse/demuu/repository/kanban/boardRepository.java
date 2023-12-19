1a33e5630353849bcc1693b75fe004d2cc8ddb30
demuu <preasim.kr@gmail.com>
2023-12-20 04:24:44 +0900

    Update boardRepository.java

--- server/demuu/src/main/java/com/apocalypse/demuu/repository/kanban/boardRepository.java
+++ server/demuu/src/main/java/com/apocalypse/demuu/repository/kanban/boardRepository.java
@@ -1,7 +1,11 @@
 package com.apocalypse.demuu.repository.kanban;
 
+import com.apocalypse.demuu.entity.Member;
 import com.apocalypse.demuu.entity.kanban.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findByMember(Member member);
 }
