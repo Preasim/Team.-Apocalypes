d9a28735122b68485750e165b0e62b349f087bd6
demuu <preasim.kr@gmail.com>
2023-12-20 04:24:52 +0900

    Update BoardMapper.java

--- server/demuu/src/main/java/com/apocalypse/demuu/mapper/kanban/BoardMapper.java
+++ server/demuu/src/main/java/com/apocalypse/demuu/mapper/kanban/BoardMapper.java
@@ -1,6 +1,7 @@
 package com.apocalypse.demuu.mapper.kanban;
 
-import com.apocalypse.demuu.dto.KanbanBoardDto;
+
+import com.apocalypse.demuu.dto.kanban.BoardDto;
 import com.apocalypse.demuu.entity.kanban.Board;
 import org.mapstruct.Mapper;
 
@@ -8,8 +9,8 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface BoardMapper {
-    Board postToBoard(KanbanBoardDto.Post post);
-    Board patchToBoard(KanbanBoardDto.Patch patch);
-    KanbanBoardDto.Response boardToResponse(Board board);
-    List<KanbanBoardDto.Response> boardsToResponses(List<Board> boards);
+    Board postToBoard(BoardDto.Post postDto);
+    Board patchToBoard(BoardDto.Patch patchDto);
+    BoardDto.Response boardToResponse(Board board);
+    List<BoardDto.Response> boardsToResponses(List<Board> boards);
 }
