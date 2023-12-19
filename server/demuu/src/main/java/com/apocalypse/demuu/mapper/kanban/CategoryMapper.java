7c58f1b105f01ec662f2140121e5335acde7307a
demuu <preasim.kr@gmail.com>
2023-12-20 04:24:50 +0900

    Update CategoryMapper.java

--- server/demuu/src/main/java/com/apocalypse/demuu/mapper/kanban/CategoryMapper.java
+++ server/demuu/src/main/java/com/apocalypse/demuu/mapper/kanban/CategoryMapper.java
@@ -1,7 +1,6 @@
 package com.apocalypse.demuu.mapper.kanban;
 
-import com.apocalypse.demuu.dto.KanbanBoardDto;
-import com.apocalypse.demuu.entity.kanban.Board;
+import com.apocalypse.demuu.dto.kanban.CategoryDto;
 import com.apocalypse.demuu.entity.kanban.Category;
 import org.mapstruct.Mapper;
 
@@ -9,8 +8,8 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
-    Category postToCategory(KanbanBoardDto.CategoryPost post);
-    Category patchToCategory(KanbanBoardDto.CategoryPatch patch);
-    KanbanBoardDto.CategoryResponse categoryToResponse(Category category);
-    List<KanbanBoardDto.CategoryResponse> categoriesToResponses(List<Category> categories);
+    Category postToCategory(CategoryDto.Post post);
+    Category patchToCategory(CategoryDto.Patch patch);
+    CategoryDto.Response categoryToResponse(Category category);
+    List<CategoryDto.Response> categoriesToResponses(List<Category> categories);
 }
