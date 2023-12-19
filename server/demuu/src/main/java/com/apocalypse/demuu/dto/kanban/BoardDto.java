b35db4c41bd7364e88bb6f86ab8749322d5f7613
demuu <preasim.kr@gmail.com>
2023-12-20 04:25:03 +0900

    Create BoardDto.java

---
+++ server/demuu/src/main/java/com/apocalypse/demuu/dto/kanban/BoardDto.java
@@ -0,0 +1,39 @@
+package com.apocalypse.demuu.dto.kanban;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import java.sql.Timestamp;
+
+public class BoardDto {
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Post {
+        @NotBlank
+        private String boardName;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class Patch {
+        @NotBlank
+        private String boardName;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class Response {
+        private long boardId;
+        private long memberId;
+        private String boardName;
+        private Timestamp createdAt;
+        private Timestamp modifiedAt;
+    }
+}
