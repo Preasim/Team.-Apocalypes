8d6dd4d2c70e391f612200e9fbcdf2d0595f2d93
demuu <preasim.kr@gmail.com>
2023-12-24 06:10:04 +0900

    Update ExceptionCode.java

--- server/demuu/src/main/java/com/apocalypse/demuu/exception/ExceptionCode.java
+++ server/demuu/src/main/java/com/apocalypse/demuu/exception/ExceptionCode.java
@@ -7,9 +7,9 @@ public enum ExceptionCode {
     MEMBER_EXISTS(409, "회원이 존재합니다"),
     MEMBER_IS_DELETED(404, "탈퇴한 회원입니다"),
     NO_PERMISSION_EDITING_POST(403,"작성자만 수정할 수 있습니다"),
-    NAME_EXISTS(409, "이미 존재하는 이름입니다."),
     BOARD_NOT_FOUND(404, "칸반보드를 찾을 수 없습니다."),
-    CATEGORY_NOT_FOUND(404, "카테고리를 찾을 수 없습니다.");
+    CATEGORY_NOT_FOUND(404, "카테고리를 찾을 수 없습니다."),
+    TASK_NOT_FOUND(404, "작업을 찾을 수 없습니다.");
 
     @Getter
     private int status;
